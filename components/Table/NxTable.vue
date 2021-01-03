e7ee2c010d0be5df072007bd28c42287eb76da61
zuramai <ahmadsaugigis@yahoo.com>
2021-01-03 10:44:19 +0700

    Added Table Variants

--- components/Table/NxTable.vue
+++ components/Table/NxTable.vue
@@ -1,19 +1,49 @@
 <template>
     <div class='nx-component'>
-        <table class="nx-table">
-            <thead>
-                <slot name="thead"/>
-            </thead>
-            <tbody>
-                <slot name="tbody"/>
-            </tbody>
-        </table>
+        <div class="nx-table-wrapper">
+            <table :class="tableClasses">
+                <thead>
+                    <slot name="thead"/>
+                </thead>
+                <tbody>
+                    <slot name="tbody"/>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 <script>
 export default {
     props: {
-
+        type: {
+            type: String,
+            default: 'hover',
+            validator: function (value) {
+                return ['hover', 'bordered', 'borderless'].indexOf(value) !== -1
+            }
+        },
+        size: {
+            type: String,
+            default: 'md',
+            validator: function (value) {
+                return ['sm', 'md', 'lg'].indexOf(value) !== -1
+            }
+        },
+        rounded: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    computed: {
+        tableClasses() {
+            return `
+                nx-table 
+                w-full 
+                rounded-lg
+                table-${this.type}
+                table-${this.size}
+            `
+        }
     }
 }
 </script>
--- pages/components/table.vue
+++ pages/components/table.vue
@@ -35,7 +35,98 @@
                                             <td>{{tableData.name}}</td>
                                             <td>{{tableData.address}}</td>
                                             <td>{{tableData.phone}}</td>
-                                            <td>{{tableData.status}}</td>
+                                            <td>
+                                                <nx-badge :color="tableData.status ? 'success' : 'danger'">{{tableData.status ? 'Active' : 'Inactive'}}</nx-badge></td>
+                                        </tr>
+                                    </template>
+                                </nx-table>
+                            </template>
+                        </nx-card>
+                        <nx-card>
+                            <template v-slot:header>
+                                <nx-card-header>
+                                    <template v-slot:title>Table Hover</template>
+                                </nx-card-header>
+                            </template>
+                            <template v-slot:body>
+                                <nx-table type="hover">
+                                    <template v-slot:thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Name</th>
+                                            <th>Address</th>
+                                            <th>Phone</th>
+                                            <th>Status</th>
+                                        </tr>
+                                    </template>
+                                    <template v-slot:tbody>
+                                        <tr v-for="tableData in tableData1" :key="tableData.id">
+                                            <td>{{tableData.id}}</td>
+                                            <td>{{tableData.name}}</td>
+                                            <td>{{tableData.address}}</td>
+                                            <td>{{tableData.phone}}</td>
+                                            <td>
+                                                <nx-badge :color="tableData.status ? 'success' : 'danger'">{{tableData.status ? 'Active' : 'Inactive'}}</nx-badge></td>
+                                        </tr>
+                                    </template>
+                                </nx-table>
+                            </template>
+                        </nx-card>
+                        <nx-card>
+                            <template v-slot:header>
+                                <nx-card-header>
+                                    <template v-slot:title>Table Striped</template>
+                                </nx-card-header>
+                            </template>
+                            <template v-slot:body>
+                                <nx-table type="striped">
+                                    <template v-slot:thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Name</th>
+                                            <th>Address</th>
+                                            <th>Phone</th>
+                                            <th>Status</th>
+                                        </tr>
+                                    </template>
+                                    <template v-slot:tbody>
+                                        <tr v-for="tableData in tableData1" :key="tableData.id">
+                                            <td>{{tableData.id}}</td>
+                                            <td>{{tableData.name}}</td>
+                                            <td>{{tableData.address}}</td>
+                                            <td>{{tableData.phone}}</td>
+                                            <td>
+                                                <nx-badge :color="tableData.status ? 'success' : 'danger'">{{tableData.status ? 'Active' : 'Inactive'}}</nx-badge></td>
+                                        </tr>
+                                    </template>
+                                </nx-table>
+                            </template>
+                        </nx-card>
+                        <nx-card>
+                            <template v-slot:header>
+                                <nx-card-header>
+                                    <template v-slot:title>Search + Sort</template>
+                                </nx-card-header>
+                            </template>
+                            <template v-slot:body>
+                                <nx-table type="striped">
+                                    <template v-slot:thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Name</th>
+                                            <th>Address</th>
+                                            <th>Phone</th>
+                                            <th>Status</th>
+                                        </tr>
+                                    </template>
+                                    <template v-slot:tbody>
+                                        <tr v-for="tableData in tableData1" :key="tableData.id">
+                                            <td>{{tableData.id}}</td>
+                                            <td>{{tableData.name}}</td>
+                                            <td>{{tableData.address}}</td>
+                                            <td>{{tableData.phone}}</td>
+                                            <td>
+                                                <nx-badge :color="tableData.status ? 'success' : 'danger'">{{tableData.status ? 'Active' : 'Inactive'}}</nx-badge></td>
                                         </tr>
                                     </template>
                                 </nx-table>
@@ -63,70 +154,35 @@ export default {
                 "name": "Castor Rose",
                 "address": "8884 Non, Rd.",
                 "phone": "061-994-4236",
-                "status": "1"
+                "status": 1
             },
             {
                 "id": 2,
                 "name": "Amery Morrison",
                 "address": "917-9673 Ultrices Rd.",
                 "phone": "031-514-4577",
-                "status": "1"
+                "status": 1
             },
             {
                 "id": 3,
                 "name": "Maxwell Monroe",
                 "address": "8234 Bibendum Av.",
                 "phone": "041-758-2151",
-                "status": "0"
+                "status": 0
             },
             {
                 "id": 4,
                 "name": "Cyrus Mayo",
                 "address": "1643 Nulla Avenue",
                 "phone": "049-270-9262",
-                "status": "1"
+                "status": 1
             },
             {
                 "id": 5,
                 "name": "Lamar Vang",
                 "address": "P.O. Box 392, 9153 Sodales Av.",
                 "phone": "075-352-8066",
-                "status": "1"
-            },
-            {
-                "id": 6,
-                "name": "Dolan Goodman",
-                "address": "1024 Sed Street",
-                "phone": "033-751-4597",
-                "status": "0"
-            },
-            {
-                "id": 7,
-                "name": "Philip Dunlap",
-                "address": "637-350 Fusce Street",
-                "phone": "035-916-3848",
-                "status": "0"
-            },
-            {
-                "id": 8,
-                "name": "Axel Willis",
-                "address": "Ap #822-5241 Libero. Ave",
-                "phone": "045-054-8091",
-                "status": "0"
-            },
-            {
-                "id": 9,
-                "name": "Grant Benjamin",
-                "address": "358-9670 Semper Rd.",
-                "phone": "081-185-6204",
-                "status": "0"
-            },
-            {
-                "id": 10,
-                "name": "Wylie Byers",
-                "address": "913 Ultrices St.",
-                "phone": "045-663-6683",
-                "status": "0"
+                "status": 1
             },
         ]
 	})
