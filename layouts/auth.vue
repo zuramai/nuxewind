603175ee54ca753f5312f00bc66047852baaadd0
productfrontenddeveloper <38668796+productfrontenddeveloper@users.noreply.github.com>
2021-01-18 08:38:37 +0300

    fix auth layouts

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,10 +1,10 @@
 <template>
     <div id="auth" class='h-screen'>
-        <div class="flex h-screen">
-            <div class="w-full lg:w-1/3">
+        <div class="h-screen grid grid-cols-3">
+            <div class="col-span-3 lg:col-span-1">
                 <Nuxt/>
             </div>
-            <div class="none lg:block lg:w-2/3">
+            <div class="hidden lg:block col-span-2">
                 <div class="auth-right w-full h-full relative bg-cover" style="background-image: url(https://images.unsplash.com/photo-1610883685547-e3f7fef8f088?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80)">
                 <span class="absolute bottom-10 left-10 text-white font-semibold">Photo by <a href="https://unsplash.com/@borkography?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Adam Borkowski</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
                 </div>
